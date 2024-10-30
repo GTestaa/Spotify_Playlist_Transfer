cbcb757c41bb2e20151234c3482849805d989ea1
Giorgio Testa <96191041+GTestaa@users.noreply.github.com>
2024-10-30 23:54:43 +0000

    Implement Spotify API Web Limits

--- spotify_playlist_transfer/transferring_functions.py
+++ spotify_playlist_transfer/transferring_functions.py
@@ -5,11 +5,12 @@ import requests
 from tenacity import retry, stop_after_attempt, wait_fixed
 import time
 
-# from pprint import pprint
+# Enter your client ID and redirect URI here. Must be the same as the one used in the Spotify Developer Dashboard.
+CLIENT_ID = ""
+REDIRECT_URI = "http://localhost:8080/callback/" # Must have callback URL to redirect to after authentication
 
-CLIENT_ID = "8fbfd57e659a411b98498f335d7c290a"
-REDIRECT_URI = "http://localhost:8080/callback/"
 
+# Retry decorator to fix Spotify's API rate limit 
 @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def add_items_to_playlist(sp, playlist_id, track_uris):
 # Add tracks to a playlist 
@@ -18,54 +19,55 @@ def add_items_to_playlist(sp, playlist_id, track_uris):
 @retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
 def download_from_origin():
     print("Login to Origin account")
-    sp_from = spotipy.Spotify(auth_manager=SpotifyPKCE(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI, scope="user-library-read"))
-    print("\n\nOrigin account:", sp_from.me()['display_name'])
+    origin_spotify = spotipy.Spotify(auth_manager=SpotifyPKCE(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI, scope="user-library-read"))
+    print("\n\nOrigin account:", origin_spotify.me()['display_name'])
 
-    # Playlists
+    # Download playlists
     print("Downloading playlists...")
-    result = sp_from.current_user_playlists()  # dict output -> list playlists -> dict playlist
-    playlists = result['items']
+    result = origin_spotify.current_user_playlists()  # current user's playlists as a dictionary
+    playlists = result['items'] # list of playlists
     while result['next']:  # pages after first
-        result = sp_from.next(result)
-        playlists.extend(result['items'])
+        result = origin_spotify.next(result) # get next page
+        playlists.extend(result['items']) # add to list of playlists
 
-    for playlist in playlists:
-        result = sp_from.playlist_tracks(playlist['id'])
-        playlist_tracks = result['items']
-        while result['next']:
-            result = sp_from.next(result)
-            playlist_tracks.extend(result['items'])
+    for playlist in playlists: # for each playlist 
+        result = origin_spotify.playlist_tracks(playlist['id']) # get tracks of playlist
+        playlist_tracks = result['items']   # list of tracks
+        while result['next']: # pages after first
+            result = origin_spotify.next(result) # get next page
+            playlist_tracks.extend(result['items']) # add to list of tracks
         track_list = []
         for track in playlist_tracks:
             track_list.append(track['track']['uri'])
         playlist['track_list'] = track_list
 
-    # Saved tracks
-    print("Downloading saved tracks...")
-    result = sp_from.current_user_saved_tracks()
-    saved_tracks = result['items']
-    while result['next']:
-        result = sp_from.next(result)
-        saved_tracks.extend(result['items'])
-    saved_tracks_list = [saved_track['track']['id'] for saved_track in saved_tracks]
+    # Download saved tracks
+    print("Downloading saved tracks...") 
+    result = origin_spotify.current_user_saved_tracks() # desired user playlist tracks saved as a dictionary
+    saved_tracks = result['items'] # list of saved tracks
+    while result['next']: # pages after first
+        result = origin_spotify.next(result) # get next page
+        saved_tracks.extend(result['items']) # add to list of saved tracks
+    saved_tracks_list = [saved_track['track']['id'] for saved_track in saved_tracks] # list of saved track IDs
 
     return (playlists, saved_tracks_list)  # return tuple of playlists and saved tracks
 
-@retry(wait=wait_fixed(30), stop=stop_after_attempt(3))
-def upload_to_destination(playlists_dict_and_saved_tracks_list_tuple):
-    playlists_dict = playlists_dict_and_saved_tracks_list_tuple[0]
-    saved_tracks_list = playlists_dict_and_saved_tracks_list_tuple[1]
+@retry(wait=wait_fixed(30), stop=stop_after_attempt(3)) 
+# upload to destination account
+def upload_to_destination(playlists_dict_and_saved_tracks_list_tuple): # takes tuple of playlists and saved tracks
+    playlists_dict = playlists_dict_and_saved_tracks_list_tuple[0] # get playlists
+    saved_tracks_list = playlists_dict_and_saved_tracks_list_tuple[1] # get saved tracks
 
     print("Login to the Spotify account you want to transfer to")
-    sp_to = spotipy.Spotify(auth_manager=SpotifyPKCE(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI, scope="playlist-modify-public playlist-modify-private ugc-image-upload user-library-modify"))
-    print("\n\nDestination account:", sp_to.me()['display_name'])
+    destination_spotify = spotipy.Spotify(auth_manager=SpotifyPKCE(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI, scope="playlist-modify-public playlist-modify-private ugc-image-upload user-library-modify"))
+    print("\n\nDestination account:", destination_spotify.me()['display_name'])
 
     # Playlists
     print("Uploading playlists...")
     for playlist in playlists_dict:
         if playlist['track_list']:
             # Create playlist
-            new_playlist = sp_to.user_playlist_create(sp_to.me()['id'], playlist['name'], public=playlist['public'], collaborative=playlist['collaborative'], description=playlist['description'])
+            new_playlist = destination_spotify.user_playlist_create(destination_spotify.me()['id'], playlist['name'], public=playlist['public'], collaborative=playlist['collaborative'], description=playlist['description'])
             print(f"Created new playlist: {playlist['name']} with ID: {new_playlist['id']}")
 
 
@@ -73,24 +75,25 @@ def upload_to_destination(playlists_dict_and_saved_tracks_list_tuple):
             if playlist['images']:
                 thumbnail_b64 = base64.b64encode(requests.get(playlist['images'][0]['url']).content)
                 try:
-                    sp_to.playlist_upload_cover_image(new_playlist['id'], thumbnail_b64)
+                    destination_spotify.playlist_upload_cover_image(new_playlist['id'], thumbnail_b64)
                     print(f"Thumbnail uploaded for playlist: {playlist['name']}")
                 except Exception as e:
                     print(f"Error uploading thumbnail of \"{playlist['name']}\" playlist; skipping... ({str(e)})")
 
             # Set tracks
-            list_49 = split_every_49(playlist['track_list'])  # Split into chunks to respect API limits
+            list_49 = song_segmentation(playlist['track_list'])  # Split into chunks to respect API limits
             for chunk in list_49:
                 print(f"Adding a chunk of {len(chunk)} tracks to playlist: {playlist['name']}")
-                add_items_to_playlist(sp_to, new_playlist['id'], chunk)
+                add_items_to_playlist(destination_spotify, new_playlist['id'], chunk)
         else:
             print(f"Playlist \"{playlist['name']}\" has no tracks; skipping...")
 
     # Saved tracks
     print("Uploading saved tracks...")
-    list_49 = split_every_49(saved_tracks_list)
+    list_49 = song_segmentation(saved_tracks_list)
     for chunk in list_49:
-        sp_to.current_user_saved_tracks_add(chunk)
+        destination_spotify.current_user_saved_tracks_add(chunk)
 
-def split_every_49(list):
+# Split list into chunks of 49 to respect Spotify API limits
+def song_segmentation(list):
     return [list[i:i + 49] for i in range(0, len(list), 49)]
